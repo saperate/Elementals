ea8e7a24e718c687011d0dfa810dd738030429a5
saperate <61527642+saperate@users.noreply.github.com>
2024-05-31 22:52:15 -0400

    Trees are almost done i just need to connect the main node

--- src/client/java/gui/UpgradeTreeScreen.java
+++ src/client/java/gui/UpgradeTreeScreen.java
@@ -53,12 +53,20 @@ public class UpgradeTreeScreen extends SpruceScreen {
         context.drawTexture(CreateWorldScreen.LIGHT_DIRT_BACKGROUND_TEXTURE, 0, 0, -oX, -oY, width, height, 32, 32);
         context.fill(oX, oY, oX + tileSize, oY + tileSize, 0xFFFFFFFF);
 
-        for (int i = 0; i < bender.element.root.children.length; i++) {
-            if(i != 0){
-                return;
-            }
-            Upgrade child = bender.element.root.children[i];
-            drawTree(child, context, oX + child.mod, oY + spacing, -1);
+        Upgrade root = bender.element.root;
+
+        int len = root.children.length;
+        if(len >= 1){
+            drawMirroredTree(root.children[0], context, oX - spacing, oY + root.children[0].mod, -1);
+        }
+        if(len >= 2){
+            drawTree(root.children[1], context, oX + root.children[0].mod, oY + spacing, 1);
+        }
+        if(len >= 3){
+            drawMirroredTree(root.children[2], context, oX + spacing, oY + root.children[2].mod, 1);
+        }
+        if(len == 4){
+            drawTree(root.children[3], context, oX + root.children[1].mod, oY - spacing, -1);
         }
     }
 
@@ -78,13 +86,16 @@ public class UpgradeTreeScreen extends SpruceScreen {
 
     //BEWARE: beyond this point is shitty code that might be hard to understand, read at your own peril traveller
     public void drawTree(Upgrade parent, DrawContext context, int oX, int oY, int mult) {
+        //Draw the node
         context.fill(oX, oY, oX + tileSize, oY + tileSize, 0xFFFFFFFF);
         if(parent.children.length == 0){
             return;
         }
+
+        //Path directly down that stops halfway to the next tile
         context.fill(
-                oX + tileSize / 2 - pathSize, oY + tileSize,
-                oX + tileSize /2 + pathSize, oY +tileSize + tileSize /2,
+                oX + tileSize / 2 - pathSize, oY + (mult > 0 ? tileSize : 0),
+                oX + tileSize /2 + pathSize, oY + (tileSize /2 * mult) + (mult > 0 ? tileSize : 0),
                 0xFF454545
         );
 
@@ -95,6 +106,7 @@ public class UpgradeTreeScreen extends SpruceScreen {
             Upgrade child = parent.children[i];
 
             int pX = oX + Math.round(child.getPositionX() * spacing);
+
             if(i == 0){
                 firstChildPosX = pX;
             }
@@ -102,18 +114,72 @@ public class UpgradeTreeScreen extends SpruceScreen {
                 lastChildPosX = pX;
             }
 
+            //Path directly up that stop halfway through
             context.fill(
-                    pX + tileSize /2 - pathSize, oY+ spacing,
-                    pX + tileSize - tileSize/2 + pathSize, oY + spacing - tileSize/2 - pathSize/2,
+                    pX + tileSize /2 - pathSize, oY + (mult > 0 ? spacing : - tileSize/2),
+                    pX + tileSize - tileSize/2 + pathSize, oY + (mult > 0 ? spacing - tileSize/2 : - tileSize) - pathSize/2,
                     0xFF454545
             );
 
-            drawTree(child, context, pX, oY + spacing, mult);
+            drawTree(child, context, pX, oY + (spacing * mult), mult);
         }
 
+        //Path is on the halfway point of this depth and the previous depth, and it spans the whole width of the path
         context.fill(
-                firstChildPosX + tileSize/2 - pathSize, oY + spacing - tileSize /2 - pathSize,
-                lastChildPosX + tileSize - tileSize/2 + pathSize, oY + spacing - tileSize /2 + pathSize,
+                firstChildPosX + tileSize/2 - pathSize, oY + (mult > 0 ? spacing : 0) -tileSize/2  - pathSize,
+                lastChildPosX + tileSize - tileSize/2 + pathSize, oY + (mult > 0 ? spacing: 0) -tileSize/2  + pathSize,
+                0xFF454545
+        );
+
+    }
+
+
+    public void drawMirroredTree(Upgrade parent, DrawContext context, int oX, int oY, int mult) {
+        //Draw the node
+        context.fill(oX, oY, oX + tileSize, oY + tileSize, 0xFFFFFFFF);
+        if(parent.children.length == 0){
+            return;
+        }
+
+
+        //Path directly down that stops halfway to the next tile
+        context.fill(
+                oX + (mult > 0 ? tileSize : 0), oY + tileSize / 2 - pathSize,
+                oX + (tileSize /2 * mult) + (mult > 0 ? tileSize : 0), oY + tileSize /2 + pathSize,
+                0xFF454545
+        );
+
+
+
+        int firstChildPosY = 0;
+        int lastChildPosY = 0;
+
+        for (int i = 0; i < parent.children.length; i++) {
+            Upgrade child = parent.children[i];
+
+            int pY = oY + Math.round(child.getPositionX() * spacing);
+
+            if(i == 0){
+                firstChildPosY = pY;
+            }
+            if (i == parent.children.length - 1) {
+                lastChildPosY = pY;
+            }
+
+            //Path directly up that stop halfway through
+            context.fill(
+                    oX + (mult > 0 ? spacing : - tileSize/2) , pY + tileSize /2 - pathSize,
+                    oX + (mult > 0 ? spacing - tileSize/2 : - tileSize) - pathSize/2, pY + tileSize - tileSize/2 + pathSize,
+                    0xFF454545
+            );
+
+            drawMirroredTree(child, context, oX + (spacing * mult), pY, mult);
+        }
+
+        //Path is on the halfway point of this depth and the previous depth, and it spans the whole width of the path
+        context.fill(
+                oX + (mult > 0 ? spacing : 0) -tileSize/2  - pathSize, firstChildPosY + tileSize/2 - pathSize,
+                oX + (mult > 0 ? spacing: 0) -tileSize/2  + pathSize, lastChildPosY + tileSize - tileSize/2 + pathSize,
                 0xFF454545
         );
 
