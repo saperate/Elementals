c2f1ec280e8483e4ca79dfa010b95bcf54781775
saperate <61527642+saperate@users.noreply.github.com>
2025-09-05 18:09:16 -0400

    Sky bison now wanders on land & air, fixed where it didnt turn midair

--- src/main/java/dev/saperate/elementals/entities/common/sky_bison/SkyBisonEntity.java
+++ src/main/java/dev/saperate/elementals/entities/common/sky_bison/SkyBisonEntity.java
@@ -1,11 +1,19 @@
 package dev.saperate.elementals.entities.common.sky_bison;
 
+import dev.saperate.elementals.utils.SapsUtils;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.MovementType;
+import net.minecraft.entity.ai.NoPenaltyTargeting;
+import net.minecraft.entity.ai.control.MoveControl;
 import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.ai.pathing.EntityNavigation;
+import net.minecraft.entity.ai.pathing.MobNavigation;
+import net.minecraft.entity.ai.pathing.PathNodeNavigator;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.mob.PathAwareEntity;
-import net.minecraft.entity.passive.AbstractHorseEntity;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -13,7 +21,14 @@ import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.trunk.BendingTrunkPlacer;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
@@ -24,47 +39,88 @@ import software.bernie.geckolib.core.animation.RawAnimation;
 import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
+import java.util.Optional;
+
 public class SkyBisonEntity extends AnimalEntity implements GeoEntity {
     private final AnimatableInstanceCache animatableInstanceCache = GeckoLibUtil.createInstanceCache(this);
     public static final RawAnimation IDLE_ANIM = RawAnimation.begin().thenPlay("idle");
     public static final RawAnimation WALK_ANIM = RawAnimation.begin().thenLoop("walk");
+    public static final TrackedData<Boolean> FLYING = DataTracker.registerData(SkyBisonEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
 
     public SkyBisonEntity(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
+        this.moveControl = new SkyBisonMoveControl(this);
+        setFlying(hasNoGravity());
     }
-    
+
     protected void initGoals() {
         this.goalSelector.add(1, new EscapeDangerGoal(this, 1.2));
+        this.goalSelector.add(2, new SwimGoal(this));
         this.goalSelector.add(4, new FollowParentGoal(this, 1.0));
-        this.goalSelector.add(6, new WanderAroundFarGoal(this, 0.7));
+        this.goalSelector.add(6, new BisonWanderAroundGoal(this));
+        this.goalSelector.add(7, new WanderAroundGoal(this,1f));
         this.goalSelector.add(7, new LookAtEntityGoal(this, PlayerEntity.class, 6.0F));
         this.goalSelector.add(8, new LookAroundGoal(this));
         this.goalSelector.add(3, new TemptGoal(this, 1.25, Ingredient.ofItems(new ItemConvertible[]{Items.APPLE}), false));
     }
 
     @Override
+    protected void initDataTracker() {
+        super.initDataTracker();
+        this.getDataTracker().startTracking(FLYING, false);
+    }
+
+//TODO stamina system for flying?
+    @Override
+    public void tick() {
+        if (hasNoGravity() && !isFlying()) {
+            setNoGravity(false);
+        } else if (!hasNoGravity() && isFlying()) {
+            setNoGravity(true);
+        }
+        if(getRandom().nextInt(9600) == 0){
+            setFlying(!isFlying());
+        }
+
+        super.tick();
+    }
+
+    @Override
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+        setFlying(true);
+        addVelocity(0,.1f,0);
+        move(MovementType.SELF,getVelocity());
+        return ActionResult.PASS;
+    }
+
+    @Override
+    public void limitFallDistance() {
+        this.fallDistance = 0;
+    }
+
+    @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
         controllerRegistrar.add(new AnimationController<>(this, "Flying", 5, this::animationPredicate));
     }
-    
+
     public static DefaultAttributeContainer.Builder createAttributes() {
         return AnimalEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 40)
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.2);
     }
-    
-    
-    //Chooses which animation to use
-    private PlayState animationPredicate(AnimationState<SkyBisonEntity> animationState){
 
-        if(animationState.isMoving()){
+
+    //Chooses which animation to use
+    private PlayState animationPredicate(AnimationState<SkyBisonEntity> animationState) {
+
+        if (animationState.isMoving()) {
             animationState.getController().setAnimation(WALK_ANIM);
-        }else{
+        } else {
             animationState.getController().setAnimation(IDLE_ANIM);
         }
         return PlayState.CONTINUE;
     }
-    
+
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return animatableInstanceCache;
@@ -76,8 +132,148 @@ public class SkyBisonEntity extends AnimalEntity implements GeoEntity {
         return null;
     }
 
-    @Override
-    public boolean isCollidable() {
-        return true;
+    public void setFlying(boolean val) {
+        this.dataTracker.set(FLYING, val);
+        setNoGravity(val);
+    }
+
+    public boolean isFlying() {
+        return this.dataTracker.get(FLYING);
+    }
+
+
+    /**
+     * This handles the bison's movement when wandering. We need our own custom one
+     * since it can both fly and walk around
+     */
+    private static class BisonWanderAroundGoal extends Goal {
+
+        private final SkyBisonEntity entity;
+        private double targetX,targetY, targetZ;
+
+        BisonWanderAroundGoal(SkyBisonEntity entity) {
+            this.entity = entity;
+        }
+
+        public boolean canStart() {
+            MoveControl moveControl = entity.getMoveControl();
+            if(!entity.isFlying()){
+                return false;
+            }else {
+                if (!moveControl.isMoving()) {
+                    return entity.getRandom().nextInt(toGoalTicks(120)) == 0;
+                } else {
+                    double x = moveControl.getTargetX() - entity.getX();
+                    double y = moveControl.getTargetY() - entity.getY();
+                    double z = moveControl.getTargetZ() - entity.getZ();
+                    double d = x * x + y * y + z * z;
+                    return d < 1.0 || d > 3600.0 // Thing below gives it a chance to abandon goal
+                            || entity.getRandom().nextInt(toGoalTicks(240)) == 0;
+                }
+            }
+        }
+
+        private boolean wanderAroundGoalCanStart(){
+            if (entity.hasPassengers()) {
+                return false;
+            } else {
+                    if (entity.getRandom().nextInt(toGoalTicks(120)) != 0) {
+                        return false;
+                    }
+
+                Vec3d vec3d = this.getWanderTarget();
+                if (vec3d == null) {
+                    return false;
+                } else {
+                    this.targetX = vec3d.x;
+                    this.targetY = vec3d.y;
+                    this.targetZ = vec3d.z;
+                    return true;
+                }
+            }
+        }
+        public void start() {
+            if(entity.isFlying()) {
+                Vec3d target = getWanderTarget();
+                if (target != null) {
+                    entity.getMoveControl().moveTo(target.x, target.y, target.z, 0.8f);
+                }
+            }else{
+                entity.getMoveControl().moveTo(targetX,targetY,targetZ,1.2f);
+            }
+        }
+
+        public void stop() {
+            entity.getNavigation().stop();
+            super.stop();
+        }
+
+        @Nullable
+        protected Vec3d getWanderTarget() {
+            if (entity.isFlying()) {
+                Random random = entity.getRandom();
+                double x = entity.getX() + (double) ((random.nextFloat() * 2.0F - 1.0F) * 16.0F);
+                double y = entity.getY() + (double) ((random.nextFloat() * 2.0F - 1.0F) * 16.0F);
+                double z = entity.getZ() + (double) ((random.nextFloat() * 2.0F - 1.0F) * 16.0F);
+                return new Vec3d(x, y, z);
+            }
+            return NoPenaltyTargeting.find(entity, 10, 7);
+        }
+    }
+
+    //Flying code taken from vanilla
+    private static class SkyBisonMoveControl extends MoveControl {
+        private final SkyBisonEntity entity;
+        private int collisionCheckCooldown;
+
+        public SkyBisonMoveControl(SkyBisonEntity entity) {
+            super(entity);
+            this.entity = entity;
+            state = State.MOVE_TO;
+        }
+
+
+        public void tick() {
+            if (state == State.MOVE_TO) {
+                if (entity.isFlying()) {
+                    if (this.collisionCheckCooldown-- <= 0) {
+                        this.collisionCheckCooldown += entity.getRandom().nextInt(5) + 2;
+                        Vec3d vec3d = getDistanceToTarget();
+                        double d = vec3d.length();
+                        vec3d = vec3d.normalize();
+                        if (this.willCollide(vec3d, MathHelper.ceil(d))) {
+                            entity.setVelocity(entity.getVelocity().add(vec3d.multiply(0.1)));
+                            entity.move(MovementType.SELF, entity.getVelocity());
+                            
+                        } else {
+                            state = State.WAIT;
+                        }
+                    }
+                }
+            }
+            
+            entity.setYaw(SapsUtils.calculateYaw(entity.getVelocity()));
+            if(!entity.isFlying()){
+                super.tick();   
+            }
+        }
+
+        private boolean willCollide(Vec3d direction, int steps) {
+            Box box = entity.getBoundingBox();
+
+            for (int i = 1; i < steps; ++i) {
+                box = box.offset(direction);
+
+                if (!entity.getWorld().isSpaceEmpty(entity, box)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public Vec3d getDistanceToTarget() {
+            return new Vec3d(this.targetX - this.entity.getX(), this.targetY - this.entity.getY(), this.targetZ - this.entity.getZ());
+        }
     }
 }
