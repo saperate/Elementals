c8aa6252ef1b89b51c5826181ce91bcd570fe639
saperate <61527642+saperate@users.noreply.github.com>
2026-02-14 00:41:35 -0500

    random stuff, not much but dont wanna lose it

--- src/main/java/dev/saperate/elementals/effects/StunnedStatusEffect.java
+++ src/main/java/dev/saperate/elementals/effects/StunnedStatusEffect.java
@@ -23,6 +23,7 @@ public class StunnedStatusEffect extends StatusEffect {
     @Override
     public boolean applyUpdateEffect(LivingEntity entity, int amplifier) {
         if(entity.age % (amplifier >= 1 ? 3 : 4) == 0){
+            //TODO blinking or seeing "stars" or blurry vision
             entity.slowMovement(Blocks.AIR.getDefaultState(), new Vec3d(0.00001f,1,0.00001f));
         }
         return true;
