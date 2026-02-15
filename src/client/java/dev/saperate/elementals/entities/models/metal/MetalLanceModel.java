4abaf1df06cbd4d6414ffb86699060060cb15d2e
saperate <61527642+saperate@users.noreply.github.com>
2026-02-14 21:51:52 -0500

    Finished metal lance model & texture

--- src/client/java/dev/saperate/elementals/entities/models/metal/MetalLanceModel.java
+++ src/client/java/dev/saperate/elementals/entities/models/metal/MetalLanceModel.java
@@ -14,28 +14,22 @@ import net.minecraft.entity.Entity;
 public class MetalLanceModel extends EntityModel<MetalLanceEntity> {
 	private final ModelPart CoreTransition;
 	private final ModelPart SpearHead;
-	private final ModelPart bb_main;
 	public MetalLanceModel(ModelPart root) {
 		this.CoreTransition = root.getChild("CoreTransition");
 		this.SpearHead = root.getChild("SpearHead");
-		this.bb_main = root.getChild("bb_main");
 	}
 	public static TexturedModelData getTexturedModelData() {
 		ModelData modelData = new ModelData();
 		ModelPartData modelPartData = modelData.getRoot();
-		ModelPartData CoreTransition = modelPartData.addChild("CoreTransition", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 24.0F, 1.0F));
+		ModelPartData CoreTransition = modelPartData.addChild("CoreTransition", ModelPartBuilder.create().uv(0, 0).cuboid(-0.5F, -2.0F, -15.0F, 1.0F, 2.0F, 29.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, 1.0F));
 
-		ModelPartData cube_r1 = CoreTransition.addChild("cube_r1", ModelPartBuilder.create().uv(0, 31).cuboid(-1.0F, -2.0F, 2.0F, 1.0F, 2.0F, 5.0F, new Dilation(0.0F)), ModelTransform.of(1.0F, 0.0F, -1.0F, 0.0F, -0.0436F, 0.0F));
+		ModelPartData SpearHead = modelPartData.addChild("SpearHead", ModelPartBuilder.create(), ModelTransform.of(0.0F, 23.0F, 12.5F, 0.0F, 0.0F, 1.5708F));
 
-		ModelPartData cube_r2 = CoreTransition.addChild("cube_r2", ModelPartBuilder.create().uv(25, 27).cuboid(-1.0F, -2.0F, 2.0F, 1.0F, 2.0F, 5.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, -1.0F, 0.0F, 0.0436F, 0.0F));
+		ModelPartData cube_r1 = SpearHead.addChild("cube_r1", ModelPartBuilder.create().uv(14, 31).cuboid(1.0F, -1.01F, -1.0F, 1.0F, 1.0F, 6.0F, new Dilation(0.01F)), ModelTransform.of(0.1F, 0.51F, -2.5F, 0.0F, -0.2094F, 0.0F));
 
-		ModelPartData SpearHead = modelPartData.addChild("SpearHead", ModelPartBuilder.create().uv(0, 19).cuboid(-1.0F, -0.5F, -5.5F, 2.0F, 1.0F, 10.0F, new Dilation(0.0F))
-				.uv(25, 19).cuboid(-2.0F, -0.499F, -4.5F, 4.0F, 1.001F, 6.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 23.0F, 12.5F, 0.0F, 0.0F, 1.5708F));
-
-		ModelPartData bb_main = modelPartData.addChild("bb_main", ModelPartBuilder.create().uv(0, 0).cuboid(-1.0F, -3.0F, -9.75F, 2.0F, 4.0F, 14.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
+		ModelPartData cube_r2 = SpearHead.addChild("cube_r2", ModelPartBuilder.create().uv(0, 31).cuboid(-2.0F, -1.01F, -1.0F, 1.0F, 1.0F, 6.0F, new Dilation(0.01F)), ModelTransform.of(-0.1F, 0.51F, -2.5F, 0.0F, 0.2094F, 0.0F));
 		return TexturedModelData.of(modelData, 64, 64);
 	}
-
 	@Override
 	public void setAngles(MetalLanceEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 	}
@@ -43,6 +37,5 @@ public class MetalLanceModel extends EntityModel<MetalLanceEntity> {
 	public void render(MatrixStack matrices, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
 		CoreTransition.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
 		SpearHead.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
-		bb_main.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
 	}
 }
