89390bc7063dbccd8ed6392ed23de15b0201696a
saperate <61527642+saperate@users.noreply.github.com>
2026-04-10 00:03:40 -0400

    Fixed bug that sometimes prevented exclusive abilities from toggling

--- src/main/java/dev/saperate/elementals/network/packets/BuyUpgradeC2SPacket.java
+++ src/main/java/dev/saperate/elementals/network/packets/BuyUpgradeC2SPacket.java
@@ -38,6 +38,10 @@ public class BuyUpgradeC2SPacket {
 
             PlayerData plrData = PlayerData.get(player);
             if (plrData.buyUpgrade(upgrade)) {
+                if(upgrade.parent.exclusive){
+                    //Will disable sister upgrades
+                    plrData.setUpgrade(upgrade,true);
+                }
                 GetUpgradeListC2SPacket.send(player);
                 SyncLevelC2SPacket.send(player);
             }
