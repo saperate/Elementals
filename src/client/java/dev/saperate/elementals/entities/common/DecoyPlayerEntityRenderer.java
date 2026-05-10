d7b5f8460c2ac9f949aba618f93960b36df545a3
saperate <61527642+saperate@users.noreply.github.com>
2026-05-09 22:59:01 -0400

    Fixed spirit projection crash

--- src/client/java/dev/saperate/elementals/entities/common/DecoyPlayerEntityRenderer.java
+++ src/client/java/dev/saperate/elementals/entities/common/DecoyPlayerEntityRenderer.java
@@ -21,6 +21,7 @@ import net.minecraft.client.texture.AbstractTexture;
 import net.minecraft.client.texture.PlayerSkinTexture;
 import net.minecraft.client.util.DefaultSkinHelper;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
@@ -50,7 +51,8 @@ public class DecoyPlayerEntityRenderer extends LivingEntityRenderer<DecoyPlayerE
     @Override
     public void render(DecoyPlayerEntity decoy, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
         super.render(decoy, f, g, matrixStack, vertexConsumerProvider, i);
-        if(decoy.getOwner().equals(ClientBender.get().player) && decoy.getFocusCamera()){
+        PlayerEntity owner = decoy.getOwner();
+        if(owner != null && decoy.getOwner().equals(ClientBender.get().player) && decoy.getFocusCamera()){
             ClientBender.get().ClientAbilityData = decoy;
         }
     }
