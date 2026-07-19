9bab02d50386342225570aef28f09b6931884b5b
Yagami <mvinicosta1103@gmail.com>
2026-07-19 12:12:46 -0300

    First step.
    Signed-off-by: Yagami <mvinicosta1103@gmail.com>

---
+++ common/src/main/java/dev/saperate/elementals/elements/water/AbilityWater6.java
@@ -0,0 +1,22 @@
+package dev.saperate.elementals.elements.water;
+
+import dev.saperate.elementals.data.Bender;
+import dev.saperate.elementals.data.PlayerData;
+import dev.saperate.elementals.elements.Ability;
+
+public class AbilityWater6 implements Ability {
+    @Override
+    public void onCall(Bender bender, long deltaT) {
+        PlayerData playerData = PlayerData.get(bender.player);
+        if (!playerData.canUseUpgrade("waterHealing")) {
+            bender.setCurrAbility(null);
+            return;
+        }
+        WaterElement.get().getAbility(15).onCall(bender, deltaT);
+    }
+
+    @Override
+    public void onRemove(Bender bender) {
+    }
+
+}
